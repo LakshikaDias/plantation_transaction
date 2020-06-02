f853edf377d5be9adf338ad7814b9f9bf861d842
Lakshika Dias <lakshika9504@gmail.com>
2020-06-02 18:03:02 +0530

    Sellers and product details (additional things)

--- lib/screens/view_seller/product_details.dart
+++ lib/screens/view_seller/product_details.dart
@@ -50,9 +50,87 @@ class _ProductDetailsState extends State<ProductDetails> {
               child: Container(
                 color: Colors.white,
                 child: Image.asset(widget.prodDetailPicture),
-
+              ),
+              footer: Container(
+                color: Colors.white70,
+                child: ListTile(
+                  leading: Text(widget.prodDetailName,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),),
+                  title: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text('\$${widget.prodDetailPrice}',
+                        style: TextStyle(color: Colors.red),),
+                      ),
+                      Expanded(
+                        child: Text(widget.prodDetailQuantity,
+                          style: TextStyle(color: Colors.red), ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: MaterialButton(
+                  onPressed: (){},
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(child: Text('Quantity'),),
+                      Expanded(child: Icon(Icons.arrow_drop_down),),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: MaterialButton(
+                  onPressed: (){},
+                  color: Colors.red,
+                  textColor: Colors.white,
+                  child: Text('Buy Now'),
+                ),
+              ),
+            ],
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Product Details', style: TextStyle(fontWeight: FontWeight.bold),),
+            subtitle: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+          ),
+          Divider(),
+          Row(
+            children: <Widget>[
+              Padding(padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+              child: Text('Product Name:', style: TextStyle(color: Colors.grey),),
+              ),
+              Padding(padding: EdgeInsets.all(5.0),
+              child: Text(widget.prodDetailName),),
+            ],
+          ),
+          //  want to do =========================================================
+          Row(
+            children: <Widget>[
+              Padding(padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+                child: Text('Seller:', style: TextStyle(color: Colors.grey),),
+              ),
+              Padding(padding: EdgeInsets.all(5.0),
+                child: Text('Seller name'),),
+            ],
+          ),
+          //  want to do=====================================================
+          Row(
+            children: <Widget>[
+              Padding(padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+                child: Text('Product Condition:', style: TextStyle(color: Colors.grey),),
+              ),
+              Padding(padding: EdgeInsets.all(5.0),
+                child: Text('New'),),
+            ],
           ),
         ],
       ),
